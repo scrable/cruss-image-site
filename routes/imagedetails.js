53d0f4567a5a86991fd0724b9bf7dc4cc2adecfd
Corey Russ <cruss.seq11@gmail.com>
2020-03-30 11:04:02 -0700

    remove html from links

--- routes/imagedetails.js
+++ routes/imagedetails.js
@@ -6,7 +6,7 @@ exports.details = function(req, res){
     connection.query('SELECT * FROM `test2`.`imageposts` WHERE id=?;', t,function(err,rows) {
         connection.query('SELECT * FROM `test2`.`comments` WHERE fk_postid=?;', t, function (err, cms) {
             if (typeof rows == 'object' && !rows.length) {
-                res.redirect('/homePage.html');
+                res.redirect('/homePage');
             } else {
                 res.render('imageDetails', {data: rows, cmnts: cms});
             }
