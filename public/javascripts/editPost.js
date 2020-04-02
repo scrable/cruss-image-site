7a2e1aeb8e6bfb3ee6e8dc2c6f0cc34bb526b783
Corey Russ <cruss.seq11@gmail.com>
2020-04-02 00:25:07 -0700

    hide old title when editing

--- public/javascripts/editPost.js
+++ public/javascripts/editPost.js
@@ -3,10 +3,12 @@ editpost = () => {
     var submitbutton = document.getElementById("submitbutton");
     var cancelbutton = document.getElementById("cancelbutton");
     var ptitle = document.getElementById("ptitle");
+    var ititle = document.getElementById("ititle");
 
     if(ptitle.style.display==="none") {
         ptitle.style.display="block";
         cancelbutton.style.display="inline-block";
+        ititle.style.display="none";
         ptitle.focus();
         submitbutton.value = "Submit";
         return false;
@@ -32,10 +34,12 @@ cancelEdit = () => {
     var cancelbutton = document.getElementById("cancelbutton");
     var ptitle = document.getElementById("ptitle");
     var submitbutton = document.getElementById("submitbutton");
+    var ititle = document.getElementById("ititle");
 
     ptitle.style.display="none";
     cancelbutton.style.display="none";
     submitbutton.value = "Edit Post";
+    ititle.style.display="block";
 
     return false;
 };
