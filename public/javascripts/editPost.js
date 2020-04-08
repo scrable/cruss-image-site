cf4452d356ce2e59638ce4b70fe36bf400d0fa5a
Corey Russ <cruss.seq11@gmail.com>
2020-04-07 19:27:08 -0700

    support for editing comments

--- public/javascripts/editPost.js
+++ public/javascripts/editPost.js
@@ -3,18 +3,29 @@ editpost = () => {
     var submitbutton = document.getElementById("submitbutton");
     var cancelbutton = document.getElementById("cancelbutton");
     var ptitle = document.getElementById("ptitle");
+    var pdesc = document.getElementById("pdesc");
     var ititle = document.getElementById("ititle");
+    var idesc = document.getElementById("idesc");
 
     if(ptitle.style.display==="none") {
+        // display inputs for new title & descriptions
         ptitle.style.display="block";
+        pdesc.style.display="block";
+
+        // show a cancel button to cancel editing
         cancelbutton.style.display="inline-block";
+
+        // hide the original title & description
         ititle.style.display="none";
+        idesc.style.display="none";
+
         ptitle.focus();
         submitbutton.value = "Submit";
         return false;
     }
     else {
         ptitle.style.display="block";
+        pdesc.style.display="block";
         return validateTitle();
     }
 };
@@ -35,11 +46,19 @@ cancelEdit = () => {
     var ptitle = document.getElementById("ptitle");
     var submitbutton = document.getElementById("submitbutton");
     var ititle = document.getElementById("ititle");
+    var pdesc = document.getElementById("pdesc");
+    var idesc = document.getElementById("idesc");
 
+    // hide the editors
     ptitle.style.display="none";
+    pdesc.style.display="none";
+
     cancelbutton.style.display="none";
     submitbutton.value = "Edit Post";
+
+    // show the title and desc
     ititle.style.display="block";
+    idesc.style.display="block";
 
     return false;
 };
--- routes/editpost.js
+++ routes/editpost.js
@@ -12,22 +12,26 @@ exports.edit = function(req, res){
     var posttitle;
     var postid;
 
-    // run if the title has changed
-    if(req.body.title) {
+    // run if we are editing
+    if(req.body.title && req.body.description) {
         connection.query('SELECT * FROM `test2`.`imageposts` WHERE id=?;', t, function (err, rows) {
-            connection.query('SELECT * FROM `test2`.`comments` WHERE fk_postid=?;', t, function (err, cms) {
+            connection.query('SELECT * FROM `test2`.`comments` WHERE fk_postid=?;', t, function () {
                 posttitle = req.body.title;
+                postdescription = req.body.description;
                 postid = rows[0].id;
                 var filter1 = [posttitle, postid];
-                connection.query('UPDATE `test2`.`imageposts` SET title=? WHERE id=?', filter1, function (err) {
-                    if (err) {
-                        console.log("Error replacing title : %s ", err);
-                    }
-                    if (typeof rows == 'object' && !rows.length) {
-                        res.redirect('/homePage');
-                    } else {
-                        imageDetails.details(req, res);
-                    }
+                var filter2 = [postdescription, postid];
+                connection.query('UPDATE `test2`.`imageposts` SET title=? WHERE id=?', filter1, function () {
+                    connection.query('UPDATE `test2`.`imageposts` SET description=? WHERE id=?', filter2, function (err) {
+                        if (err) {
+                            console.log("Error replacing title : %s ", err);
+                        }
+                        if (typeof rows == 'object' && !rows.length) {
+                            res.redirect('/homePage');
+                        } else {
+                            imageDetails.details(req, res);
+                        }
+                    });
                 });
             });
         });
